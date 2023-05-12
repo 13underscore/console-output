2ad573fd0d5c27f86f128953a1a8d3b99b864977
underscore <67348353+13underscore@users.noreply.github.com>
2023-05-12 14:39:51 -0400

    Add files via upload

---
+++ main-dev.js
@@ -0,0 +1,64 @@
+// create a style element
+const style = document.createElement('style');
+
+// add the CSS as a string using template literals
+style.appendChild(document.createTextNode(`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap');
+  @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css');
+  #console-div { 
+    font-family: 'Montserrat', sans-serif;
+    color: white;
+    background-color: #232323;
+    width: 46rem;
+    border-radius: 0.5%;
+  }
+  #console-output {
+    height: 18rem;
+    background-color: #323232;
+    border-radius: 0.5%;
+    overflow: auto;
+  }
+  #console-input {
+    font-family: Arial;
+  }`
+));
+
+// add it to the head
+const head = document.getElementsByTagName('head')[0];
+head.appendChild(style);
+
+
+
+const origConsoleLog = console.log;
+console.log = (...args) => {
+  origConsoleLog.apply(console, args);
+  MessageAdd(args, "white")
+};
+
+
+console.log('foo');
+console.log('bar', 'baz');
+
+const origConsoleErrorLog = console.error;
+console.error = (...args) => {
+  origConsoleErrorLog.apply(console, args);
+  MessageAdd(args, "red")
+};
+
+
+console.error('foo');
+console.error('bar', 'baz');
+
+window.onerror = function (error, source, lineno, colno, error) {
+    MessageAdd(error, "red")
+}
+
+// add message to chat:
+function MessageAdd(message, color) {
+    var console_messages = document.getElementById("console-output");
+
+    const mesparag = document.createElement('p');
+    mesparag.appendChild(document.createTextNode(message));
+    mesparag.setAttribute("style", `color: ${color};`);
+    console_messages.appendChild(mesparag);
+}
